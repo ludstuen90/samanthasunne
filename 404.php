43f9bc4388d4ec4ca8c5adf1fb390eee2a380e3d
Lukas Udstuen <Lukas.Udstuen@gmail.com>
2018-12-13 14:14:30 -0300

    meeting fixes

--- 404.php
+++ 404.php
@@ -14,7 +14,7 @@ get_header(); ?>
 
 			<section class="error-404 not-found">
 				<header class="page-header">
-					<h1 class="page-title"><?php _e( "Uh oh! There doesn't seem to be anything here.", 'twentysixteen' ); ?></h1>
+					<h1 class="page-title"><?php _e( "Uh oh! There doesn't seem to be anything here.gi", 'twentysixteen' ); ?></h1>
 				</header><!-- .page-header -->
 
 				<div class="page-content">
