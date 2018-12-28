09ca93a9c57e98e0fe93429691ad7ad3f723e2df
Lukas Udstuen <Lukas.Udstuen@gmail.com>
2018-12-28 19:01:10 -0300

    fixed by updating image size

---
+++ template-parts/content-tutorials.php
@@ -0,0 +1,176 @@
+<?php ?>
+
+
+<style>
+.container {
+  width: 175%;
+}
+
+</style>
+
+ <div class="container">
+   <div class="row">
+     <div class="col-lg-5">
+     <h1>Tutorials</h1>
+     <br><br>
+     <img src="/wp-content/uploads/2016/10/170622_ohara_ire09_small-1200x800.jpg">
+
+
+     </div>
+
+     <div class="col-lg-7">
+       <br>
+     <p>Those great big fluffy clouds. I thought today we would make a happy little stream that's just running through the woods here. See how easy it is to create a little tree right in your world. The light is your friend. Preserve it. If you hypnotize it, it will go away.</p>
+     <p><b>Upcoming Appearances:</p></b>
+     <table>
+<tbody>
+<tr>
+<td><a href="https://featuresjournalism.org/tag/new-orleans/">Features Journalism Conference</a></td>
+<td>New Orleans, LA</td>
+<td>September 2018</td>
+</tr>
+<tr>
+<td><a href="https://www.meetup.com/Hacks-Hackers-New-Orleans/events/254793903/">Hacks/Hackers New Orleans</a></td>
+<td>New Orleans, LA</td>
+<td>September 2018</td>
+</tr>
+<tr>
+<td><a href="https://jealouisiana.org/750/state-conferences/speaker-list-jea-fall-2018-conference/">Louisiana Journalism Education Association</a></td>
+<td>New Orleans, LA</td>
+<td>September 2018</td>
+</tr>
+<tr>
+<td><a href="https://www.spj.org/election18-milwaukee.asp">Marquette University</a></td>
+<td>Milwaukee, WI</td>
+<td>October 2018</td>
+</tr>
+<tr>
+<td><a href="https://www.spj.org/election18-msp.asp">Twin Cities PBS</a></td>
+<td>St. Paul, MN</td>
+<td>October 2018</td>
+</tr>
+<tr>
+<td><a href="https://www.spj.org/election18-miami.asp">Florida International University</a></td>
+<td>Miami, FL</td>
+<td>October 2018</td>
+</tr>
+<tr>
+<td><a href="https://www.spj.org/election18-morgantown.asp">West Virginia University</a></td>
+<td>Morgantown, WV</td>
+<td>October 2018</td>
+</tr>
+</tbody>
+</table>
+
+     </div>
+   </div>
+
+   <div class="row">
+    <div class="col-md-12">
+      <h1>Online Tutorials</h1>
+    </div>
+   </div>
+
+   <div class="row">
+     <div class="col-md-4">
+       <img style="width:100%" src="http://localhost/wp-content/uploads/2011/07/bilde1.jpg">
+       <p>Just a happy little shadow that lives in there. We don't make mistakes
+         we just have happy little accidents. We need a shadow side and a
+         highlight side. The first step to doing anything is to believe
+         you can do it. See it finished in your mind before you ever start.
+         Fluff that up.</p>
+
+         <p><a href="_blank">Read More</a></p>
+
+     </div>
+     <div class="col-md-4">
+
+       <img style="width:100%" src="http://localhost/wp-content/uploads/2010/09/president010.jpg">
+       <p>Just a happy little shadow that lives in there. We don't make mistakes
+         we just have happy little accidents. We need a shadow side and a
+         highlight side. The first step to doing anything is to believe
+         you can do it. See it finished in your mind before you ever start.
+         Fluff that up.</p>
+         <p><a href=""="_blank">Read More</a></p>
+     </div>
+     <div style="width:100%" class="col-md-4">
+       <img src="http://localhost/wp-content/uploads/2011/07/fish2.jpg">
+       <p>Just a happy little shadow that lives in there. We don't make mistakes
+         we just have happy little accidents. We need a shadow side and a
+         highlight side. The first step to doing anything is to believe
+         you can do it. See it finished in your mind before you ever start.
+         Fluff that up.</p>
+         <p><a href="_blank">Read More</a></p>
+     </div>
+   </div>
+
+
+   <div class="row">
+     <div class="col-md-4">
+       <img style="width:100%" src="http://localhost/wp-content/uploads/2011/07/bilde1.jpg">
+       <p>Just a happy little shadow that lives in there. We don't make mistakes
+         we just have happy little accidents. We need a shadow side and a
+         highlight side. The first step to doing anything is to believe
+         you can do it. See it finished in your mind before you ever start.
+         Fluff that up.</p>
+
+         <p><a href="_blank">Read More</a></p>
+
+     </div>
+     <div class="col-md-4">
+
+       <img style="width:100%" src="http://localhost/wp-content/uploads/2010/09/president010.jpg">
+       <p>Just a happy little shadow that lives in there. We don't make mistakes
+         we just have happy little accidents. We need a shadow side and a
+         highlight side. The first step to doing anything is to believe
+         you can do it. See it finished in your mind before you ever start.
+         Fluff that up.</p>
+         <p><a href=""="_blank">Read More</a></p>
+     </div>
+     <div style="width:100%" class="col-md-4">
+       <img src="http://localhost/wp-content/uploads/2011/07/fish2.jpg">
+       <p>Just a happy little shadow that lives in there. We don't make mistakes
+         we just have happy little accidents. We need a shadow side and a
+         highlight side. The first step to doing anything is to believe
+         you can do it. See it finished in your mind before you ever start.
+         Fluff that up.</p>
+         <p><a href="_blank">Read More</a></p>
+     </div>
+   </div>
+
+   <div class="row">
+     <div class="col-md-4">
+       <img style="width:100%" src="http://localhost/wp-content/uploads/2011/07/bilde1.jpg">
+       <p>Just a happy little shadow that lives in there. We don't make mistakes
+         we just have happy little accidents. We need a shadow side and a
+         highlight side. The first step to doing anything is to believe
+         you can do it. See it finished in your mind before you ever start.
+         Fluff that up.</p>
+
+         <p><a href="_blank">Read More</a></p>
+     </div>
+     <div class="col-md-4">
+
+       <img style="width:100%" src="http://localhost/wp-content/uploads/2010/09/president010.jpg">
+       <p>Just a happy little shadow that lives in there. We don't make mistakes
+         we just have happy little accidents. We need a shadow side and a
+         highlight side. The first step to doing anything is to believe
+         you can do it. See it finished in your mind before you ever start.
+         Fluff that up.</p>
+         <p><a href=""="_blank">Read More</a></p>
+     </div>
+     <div style="width:100%" class="col-md-4">
+       <img src="http://localhost/wp-content/uploads/2011/07/fish2.jpg">
+       <p>Just a happy little shadow that lives in there. We don't make mistakes
+         we just have happy little accidents. We need a shadow side and a
+         highlight side. The first step to doing anything is to believe
+         you can do it. See it finished in your mind before you ever start.
+         Fluff that up.</p>
+         <p><a href="_blank">Read More</a></p>
+     </div>
+   </div>
+
+
+
+
+ </div>
