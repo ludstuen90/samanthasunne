f4c00446e9daf4d85d5bb6fcc027d23f81eb6bc5
Lukas Udstuen <lukas.udstuen@gmail.com>
2020-05-16 19:54:21 -0300

    add banner

--- template-parts/content-tutorials.php
+++ template-parts/content-tutorials.php
@@ -59,13 +59,24 @@
 
      </div>
    </div>
+ </div>
 <br>
+
+<div class="news_banner news_div">
+  <img class="news_banner" src="https://www.samanthasunne.com/wp-content/uploads/2020/05/BU_rectangle_116.png" ></img>
+  <img class="news_banner" src="https://www.samanthasunne.com/wp-content/uploads/2020/05/au_rectangle_116.png" ></img>
+  <img class="news_banner" src="https://www.samanthasunne.com/wp-content/uploads/2020/05/spj_rectangle_116.png" ></img>
+  <img class="news_banner" src="https://www.samanthasunne.com/wp-content/uploads/2020/05/gni_rectangle_116.png"</img>
+  <img class="news_banner" src="https://www.samanthasunne.com/wp-content/uploads/2020/05/IRE_rectangle_116.png" </img>
+</div>
+<br>
+
+<div class="container entry-content">
    <div class="row">
     <div class="col-md-12">
       <h1>Online Tutorials</h1>
     </div>
    </div>
-
 <!-- This is Row 1-->
    <div class="row">
 
@@ -94,7 +105,7 @@
 
 <!-- This is Row 2-->
 	 <div class="row">
-    
+
     <div class="col-md-4">
        <a href="https://docs.google.com/presentation/d/e/2PACX-1vQMTheumShdIjFbGnSsStI0SS6Jgz6LsVBWt1u0GIjlmr2vymIe3RwjVWYB8BV-YQqvyxP9iLQClntM/pub?start=false&loop=false&delayms=3000"><img style="width:100%" src="https://www.samanthasunne.com/wp-content/uploads/2020/02/Screen-Shot-2020-02-19-at-10.36.40-AM.png"></a>
      <a href="https://docs.google.com/presentation/d/e/2PACX-1vQMTheumShdIjFbGnSsStI0SS6Jgz6LsVBWt1u0GIjlmr2vymIe3RwjVWYB8BV-YQqvyxP9iLQClntM/pub?start=false&loop=false&delayms=3000"><b>Make Your Own Google Map</b></a>
@@ -136,7 +147,7 @@
      <p>Misinformation is a growing force in the digital-first world of journalism, and a source of some panic from reporters and audiences alike. This talk from 2017 outlines a few paths forward, including making attempts to understand the motivation behind fake news-spreaders.</p>
 <p>This talk is also available as a video from <a href="https://media.lugons.org/BalCCon2k17/BalCCon2k17_Samantha_Sunne_-_Hacking_for_Journalism.mp4">BalCCon</a>.</p>
      </div>
-     
+
      <div class="col-md-4">
      <a href="https://gijn.org/2018/05/28/what-to-watch-for-in-the-coming-wave-of-deep-fake-videos/"><img style="width:100%" src="https://www.samanthasunne.com/wp-content/uploads/2019/01/deep-fakes.jpg"></a>
      <a href="https://gijn.org/2018/05/28/what-to-watch-for-in-the-coming-wave-of-deep-fake-videos/"><b>What to look for in deep fake videos</b></a>
@@ -164,6 +175,6 @@
        <p>University campuses provide a unique hunting ground for investigative stories. This tipsheet explains some of the most common and accessible sources for breaking in to these closed communities, including CLERY reports, Title IX complaints, FERPA exceptions and FBI UCR statistics.</p>
 <p>Warning: this tipsheet was published in 2014 and may be out of date.</p>
      </div>
-     
+
  </div>
  <!-- This is the end of Row 4-->
