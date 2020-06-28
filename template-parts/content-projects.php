102089ffb115bf65a1b83430e7b21f3f308e3856
Lukas Udstuen <Lukas.Udstuen@gmail.com>
2020-06-27 23:01:22 -0300

    content projects

--- template-parts/content-projects.php
+++ template-parts/content-projects.php
@@ -11,14 +11,14 @@
   <div class="row">
     <div class="col-md-6">
         <h5 class="project-title">Investigative reporting</h5>
-		<p><img class="project_image" src="https://www.samanthasunne.com/wp-content/uploads/2019/01/la.jpg"></p>
+		<p><img class="project_image" src="../wp-content/uploads/2020/06/la.jpg"></p>
 		<p>I publish investigative stories in New Orleans, Louisiana, where I have been freelancing for four years. Recent accolades include second place for Investigative Reporting in the New Orleans Press Club and the Silicon Bayou 100 most influential people in Louisiana tech.</p>
 		<p>You can find my stories on <a href="samanthasunne.contently.com">my Contently portfolio</a> and <a href="mailto:samanthasunne@gmail.com">contact me</a> for collaborations.</p>
       </div>
 
       <div class="col-md-6">
         <h5 class="project-title">Tools for Reporters</h5>
-		  <a href="https://toolsforreporters.com/"><img class="project_image" src="https://www.samanthasunne.com/wp-content/uploads/2019/01/logo447x150.jpg"></a>
+		  <a href="https://toolsforreporters.com/"><img class="project_image" src="../wp-content/uploads/2020/06/logo447x150.jpg"></a>
 		  <p>I publish a biweekly newsletter that introduces journalists to a useful new tool every other week. <a href="https://toolsforreporters.com/">Tools for Reporters</a> is an approachable, hands-on way to keep up with rapid developments in digital journalism.</p>
 		  <p>TFR was named one of the <a href="https://blog.getrevue.co/the-most-popular-media-newsletters-b5886a8d5b9a">most popular media newsletters</a> of 2019 and has been recommended by Poynter, SPJ, and the Lenfest Institute.</p>
         <p>📫 Subscribe to <a href="https://toolsforreporters.substack.com/subscribe">Tools for Reporters</a></p>
@@ -28,14 +28,14 @@
   <div class="row">
     <div class="col-md-6">
       <h5 class="project-title">Teaching and training</h5>
-      <img class="project_image" src="https://www.samanthasunne.com/wp-content/uploads/2019/01/talking.jpg">
+      <img class="project_image" src="../wp-content/uploads/2020/06/talking.jpg">
       <p>I teach journalism tools and techniques at conferences, universities, newsrooms and professional gatherings around the world. Training partners include IRE, SPJ, the Google News Initiative, Boston University and American University in Washington, DC.</p>
 		<p>You can <a href="/tutorials/">find my tutorials here</a> or <a href="mailto:samanthasunne@gmail.com">contact me</a> for speaking engagements.</p>
 	  </div>
 
     <div class="col-md-6">
       <h5 class="project-title">Hacks/Hackers</h5>
-      <img class="project_image" src="https://www.samanthasunne.com/wp-content/uploads/2019/01/hackshackers_logotype-stacked.png">
+      <img class="project_image" src="../wp-content/uploads/2020/06/hackshackers_logotype-stacked.png">
       <p>I manage communications for <a href="https://hackshackers.com">Hacks/Hackers</a>, the world's largest journalism/tech association with more than 100 chapters around the globe. In recent years I have helped launch new groups in Rome, Jakarta, Melbourne and Benin, Nigeria. In 2016, I co-founded the <a href="https://www.meetup.com/Hacks-Hackers-New-Orleans/">local Hacks/Hackers New Orleans chapter</a>.</p>
 		<p>If you have questions about Hacks/Hackers or would like to join or start a group, please <a href="mailto:samantha@hackshackers.com">contact me.</a></p>
 		<p>🏘 Join <a href="https://www.meetup.com/Hacks-Hackers-New-Orleans/">Hacks/Hackers New Orleans</a></p>
@@ -45,13 +45,13 @@
   <div class="row">
     <div class="col-md-6">
       <h5 class="project-title">MisinfoCon</h5>
-      <img class="project_image" src="https://www.samanthasunne.com/wp-content/uploads/2019/01/misinfocon_yellow.png">
+      <img class="project_image" src="../wp-content/uploads/2020/06/misinfocon_yellow.png">
       <p>I am the managing editor of <a href="https://misinfocon.com">MisinfoCon.com</a>, a curation of essays and experiments targeting the current misinformation crisis. My anti-misinformation advocacy has included speaking on a panel in Moscow, organizing conferences, traveling to Harvard to brainstorm best practices with other trainers. In 2017, I gave a talk about threat modeling against misinformation in Serbia.</p>
 		<p>✍️ Send me your pitches for <a href="https://docs.google.com/forms/d/e/1FAIpQLSfYC27JfyoLT4UkdneXWxtx6mLNHFKzpqdanYxlSxPefArgcA/viewform?c=0&w=1">MisinfoCon</a></p>
     </div>
     <div class="col-md-6">
       <h5 class="project-title">Articles for journalists</h5>
-        <img class="project_image" src="https://www.samanthasunne.com/wp-content/uploads/2019/01/articles.jpeg">
+        <img class="project_image" src="../wp-content/uploads/2020/06/articles.jpeg">
       <p>I occasionally write articles for journalists in industry publications. These include Poynter, OpenNews, the Global Investigative Journalism Network and other sites. My articles can be found on <a href="https://samanthasunne.contently.com">my Contently portfolio</a>, and you can <a href="mailto:samanthasunne@gmail.com">email me</a> for freelance work.</p>
     </div>
   </div>
@@ -60,7 +60,7 @@
   <div class="row">
     <div class="col-md-6">
       <h5 class="project-title">Unicode</h5>
-      <img class="project_image" src="https://www.samanthasunne.com/wp-content/uploads/2020/04/unicode-e1586209134419.png">
+      <img class="project_image" src="../wp-content/uploads/2020/06/unicode-e1586209134419.png">
 		<p>I work with Unicode and Emojination to create emoji that better represent different cultures, religions and identities around the world. I proposed seven emoji that will be added to the <a href="https://unicode.org/emoji/charts/full-emoji-list.html">Unicode library</a> in 2020, including the Russian nesting doll, African hut and polar bear.</p>
 		<p>🧐 Suggest a <a href="http://www.emojination.org">new emoji</a></p>
     </div>
