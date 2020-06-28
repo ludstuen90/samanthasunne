85aae56fe43592a5efe8a1afc36fb660815280a4
Lukas Udstuen <Lukas.Udstuen@gmail.com>
2020-06-27 22:46:14 -0300

    update content tutorials urls

--- template-parts/content-tutorials.php
+++ template-parts/content-tutorials.php
@@ -9,7 +9,7 @@
 <meta name="twitter:site" content="@samanthasunne" />
 <meta name="twitter:title" content="Samantha Sunne - Tutorials" />
 <meta name="twitter:description" content="Tutorials by Samantha Sunne, journalism trainer and freelancer." />
-<meta name="twitter:image" content="https://www.samanthasunne.com/wp-content/uploads/2016/10/170622_ohara_ire09_small-1200x800.jpg" />
+<meta name="twitter:image" content="wp-content/uploads/2020/06/170622_ohara_ire09_small-1200x800-1.jpg" />
 
 <style>
 .container {
@@ -61,11 +61,11 @@
  </div>
 
 <div class="news_banner news_div">
-  <img class="news_banner" src="https://www.samanthasunne.com/wp-content/uploads/2020/05/BU_rectangle_116.png" ></img>
-  <img class="news_banner" src="https://www.samanthasunne.com/wp-content/uploads/2020/05/au_rectangle_116.png" ></img>
-  <img class="news_banner" src="https://www.samanthasunne.com/wp-content/uploads/2020/05/spj_rectangle_116.png" ></img>
-  <img class="news_banner" src="https://www.samanthasunne.com/wp-content/uploads/2020/05/gni_rectangle_116.png"</img>
-  <img class="news_banner" src="https://www.samanthasunne.com/wp-content/uploads/2020/05/IRE_rectangle_116.png" </img>
+  <img class="news_banner" src="wp-content/uploads/2020/06/BU_rectangle_116.png" ></img>
+  <img class="news_banner" src="wp-content/uploads/2020/06/au_rectangle_116.png" ></img>
+  <img class="news_banner" src="wp-content/uploads/2020/06/spj_rectangle_116.png" ></img>
+  <img class="news_banner" src="wp-content/uploads/2020/06/gni_rectangle_116.png"</img>
+  <img class="news_banner" src="wp-content/uploads/2020/06/IRE_rectangle_116.png" </img>
 </div>
 <br>
 
@@ -79,21 +79,21 @@
    <div class="row">
 
      <div class="col-md-4">
-		 <a href="https://docs.google.com/presentation/d/1zMvbeYhZZmv_PBsXIJAvwjr9i2UujEgp76DjQOA_-u4/edit?usp=sharing"><img style="width:100%" src="https://www.samanthasunne.com/wp-content/uploads/2019/01/scraping2.png"></a>
+		 <a href="https://docs.google.com/presentation/d/1zMvbeYhZZmv_PBsXIJAvwjr9i2UujEgp76DjQOA_-u4/edit?usp=sharing"><img style="width:100%" src="wp-content/uploads/2020/06/scraping2.png"></a>
 		 <a href="https://docs.google.com/presentation/d/1zMvbeYhZZmv_PBsXIJAvwjr9i2UujEgp76DjQOA_-u4/edit?usp=sharing"><b>Scraping without Programming</b></a>
        <p>"Scraping" is a catch-all word for grabbing information off a web page and into your spreadsheet - whether the website wants you to or not.</p>
 		 <p>This tutorial is also available <a href="https://cn.gijn.org/2018/05/09/%E7%BB%99%E7%BC%96%E7%A8%8B%E5%B0%8F%E7%99%BD%E7%9A%84%E6%95%B0%E6%8D%AE%E6%8A%93%E5%8F%96%E6%95%99%E7%A8%8B/">in Chinese</a> and as a video on <a href="https://learn.ire.org/all-videos/">NICAR-LEARN</a>.</p>
      </div>
 
      <div class="col-md-4">
-		 <a href="https://docs.google.com/presentation/d/1282wSn21GLcGD7xwJ3xtRp-ECswjp5npJ_Ap0cxWGUE/edit?usp=sharing"><img style="width:100%" src="https://www.samanthasunne.com/wp-content/uploads/2019/06/spreadsheet.png"></a>
+		 <a href="https://docs.google.com/presentation/d/1282wSn21GLcGD7xwJ3xtRp-ECswjp5npJ_Ap0cxWGUE/edit?usp=sharing"><img style="width:100%" src="wp-content/uploads/2020/06/spreadsheet.png"></a>
 		 <a href="https://docs.google.com/presentation/d/1282wSn21GLcGD7xwJ3xtRp-ECswjp5npJ_Ap0cxWGUE/edit?usp=sharing"><b>How to Use a Spreadsheet</b></a>
        <p>You've probably heard that you should use data for reporting, and here is where to start! We'll do some basic data analysis using Google Sheets.</p>
 		 <p>This tutorial was originally presented at the Boston University <a href="https://combeyond.bu.edu/workshop/data-narrative/">Data + Narrative seminar</a>.</p>
     </div>
 
       <div class="col-md-4">
-       <a href="https://docs.google.com/presentation/d/1H6dnsR5mAAkWp9UtXINkKrFfLOFECHiN24aLHxgcpBQ/edit?usp=sharing"><img style="width:100%" src="https://www.samanthasunne.com/wp-content/uploads/2020/02/Screen-Shot-2020-02-19-at-10.32.10-AM.png"></a>
+       <a href="https://docs.google.com/presentation/d/1H6dnsR5mAAkWp9UtXINkKrFfLOFECHiN24aLHxgcpBQ/edit?usp=sharing"><img style="width:100%" src="wp-content/uploads/2020/06/Screen-Shot-2020-02-19-at-10.32.10-AM.png"></a>
      <a href="https://docs.google.com/presentation/d/1H6dnsR5mAAkWp9UtXINkKrFfLOFECHiN24aLHxgcpBQ/edit?usp=sharing"><b>Backgrounding: Why? How?</b></a>
        <p>When should you background a source? How do you do it effectively, and how deep do you go? This walkthrough offers some guidance.</p>
      <p>This webinar was created for the University of Missouri Investigative Reporting class in 2020.</p>
@@ -105,21 +105,21 @@
 	 <div class="row">
 
     <div class="col-md-4">
-       <a href="https://docs.google.com/presentation/d/e/2PACX-1vQMTheumShdIjFbGnSsStI0SS6Jgz6LsVBWt1u0GIjlmr2vymIe3RwjVWYB8BV-YQqvyxP9iLQClntM/pub?start=false&loop=false&delayms=3000"><img style="width:100%" src="https://www.samanthasunne.com/wp-content/uploads/2020/02/Screen-Shot-2020-02-19-at-10.36.40-AM.png"></a>
+       <a href="https://docs.google.com/presentation/d/e/2PACX-1vQMTheumShdIjFbGnSsStI0SS6Jgz6LsVBWt1u0GIjlmr2vymIe3RwjVWYB8BV-YQqvyxP9iLQClntM/pub?start=false&loop=false&delayms=3000"><img style="width:100%" src="wp-content/uploads/2020/06/Screen-Shot-2020-02-19-at-10.36.40-AM.png"></a>
      <a href="https://docs.google.com/presentation/d/e/2PACX-1vQMTheumShdIjFbGnSsStI0SS6Jgz6LsVBWt1u0GIjlmr2vymIe3RwjVWYB8BV-YQqvyxP9iLQClntM/pub?start=false&loop=false&delayms=3000"><b>Make Your Own Google Map</b></a>
        <p>Did you know you can make your own Google Map - in 5 to 10 minutes? This tutorial shows you how, plus advanced options like customization and data organization.</p>
      <p>This guide was created for the Boston University <a href="https://combeyond.bu.edu/workshop/data-narrative/">Data + Narrative workshop</a>.</p>
      </div>
 
     <div class="col-md-4">
-       <a href="https://gijn.org/2018/11/21/a-tiered-approach-to-researching-and-pitching-investigative-stories-for-freelance-writers/"><img style="width:100%" src="https://www.samanthasunne.com/wp-content/uploads/2019/01/tiers.png"></a>
+       <a href="https://gijn.org/2018/11/21/a-tiered-approach-to-researching-and-pitching-investigative-stories-for-freelance-writers/"><img style="width:100%" src="wp-content/uploads/2020/06/tiers.png"></a>
      <a href="https://gijn.org/2018/11/21/a-tiered-approach-to-researching-and-pitching-investigative-stories-for-freelance-writers/"><b>Research tiers for freelance investigations</b></a>
        <p>An experimental guide to maximize success and minimize risk for  freelancers. Use this system of tiers to decide whether to pursue an investigation.</p>
      <p>This article is also available as a presentation from <a href="https://www.ire.org/resource-center/tipsheets">NICAR</a>.</p>
      </div>
 
     <div class="col-md-4">
-       <a href="https://docs.google.com/presentation/d/e/2PACX-1vRqQZI5RlNzXYGyuq_Wkv0Av1rNBUH1-kjLvc7pIIHOYMTxcYckWhZBwW1_Oah07VCSdbIwny0YEOkd/pub?start=false&loop=false&delayms=3000"><img style="width:100%" src="https://www.samanthasunne.com/wp-content/uploads/2020/02/Screen-Shot-2020-02-19-at-10.42.04-AM.png"></a>
+       <a href="https://docs.google.com/presentation/d/e/2PACX-1vRqQZI5RlNzXYGyuq_Wkv0Av1rNBUH1-kjLvc7pIIHOYMTxcYckWhZBwW1_Oah07VCSdbIwny0YEOkd/pub?start=false&loop=false&delayms=3000"><img style="width:100%" src="wp-content/uploads/2020/06/Screen-Shot-2020-02-19-at-10.42.04-AM.png"></a>
      <a href="https://docs.google.com/presentation/d/e/2PACX-1vRqQZI5RlNzXYGyuq_Wkv0Av1rNBUH1-kjLvc7pIIHOYMTxcYckWhZBwW1_Oah07VCSdbIwny0YEOkd/pub?start=false&loop=false&delayms=3000"><b>Unlocking Data from Documents</b></a>
        <p>It's a pain: you get a document, and often it's got words or data in it that you can't get out! This tutorial introduces you to several powerful tools for scraping, OCRing and text cleaning thousands or even millions of documents.</p>
      <!--There is no second line for this presentation.
@@ -133,21 +133,21 @@
   <div class="row">
 
     <div class="col-md-4">
-       <a href="https://docs.google.com/document/d/1CPu_luGTXvm3X9w4UrDgX9jIIJ21FDYinHi2z3ki6w4/edit?usp=sharing"><img style="width:100%" src="https://www.samanthasunne.com/wp-content/uploads/2019/01/tools1.png"></a>
+       <a href="https://docs.google.com/document/d/1CPu_luGTXvm3X9w4UrDgX9jIIJ21FDYinHi2z3ki6w4/edit?usp=sharing"><img style="width:100%" src="wp-content/uploads/2020/06/tools1.png"></a>
 <a href="https://docs.google.com/document/d/1CPu_luGTXvm3X9w4UrDgX9jIIJ21FDYinHi2z3ki6w4/edit?usp=sharing"><b>Best tools and apps for reporters (2018)</b></a>
        <p>Reporters are asked to do more now than they were before the digital age, but luckily, there is an almost limitless supply of tools with which to do them. Some of our favorite tools for transcribing audio, scraping PDFs and everything in between.</p>
 <p>This talk is also available as slides from <a href="https://docs.google.com/presentation/d/e/2PACX-1vQI5eu69uRgfZCThDBzmLBuXU9BK_SUGOwUx55Za3xk7ARGGu3ROfc_z1xErewgL220n0YJWGknMAr2/pub?start=false&loop=false&delayms=60000&slide=id.p">IRE</a>.</p>
      </div>
 
      <div class="col-md-4">
-     <a href="https://docs.google.com/presentation/d/195chJ9KB4gaLSNfbrWAxIUmleYyE5WrigtQN01Iqjoc/edit?usp=sharing"><img src="https://www.samanthasunne.com/wp-content/uploads/2019/01/balccon1.png"></a>
+     <a href="https://docs.google.com/presentation/d/195chJ9KB4gaLSNfbrWAxIUmleYyE5WrigtQN01Iqjoc/edit?usp=sharing"><img src="wp-content/uploads/2020/06/balccon1.png"></a>
        <a href="https://docs.google.com/presentation/d/195chJ9KB4gaLSNfbrWAxIUmleYyE5WrigtQN01Iqjoc/edit?usp=sharing"><b>Threat Modeling against Fake News</b></a>
      <p>Misinformation is a growing force in the digital-first world of journalism, and a source of some panic from reporters and audiences alike. This talk from 2017 outlines a few paths forward, including making attempts to understand the motivation behind fake news-spreaders.</p>
 <p>This talk is also available as a video from <a href="https://media.lugons.org/BalCCon2k17/BalCCon2k17_Samantha_Sunne_-_Hacking_for_Journalism.mp4">BalCCon</a>.</p>
      </div>
 
      <div class="col-md-4">
-     <a href="https://gijn.org/2018/05/28/what-to-watch-for-in-the-coming-wave-of-deep-fake-videos/"><img style="width:100%" src="https://www.samanthasunne.com/wp-content/uploads/2019/01/deep-fakes.jpg"></a>
+     <a href="https://gijn.org/2018/05/28/what-to-watch-for-in-the-coming-wave-of-deep-fake-videos/"><img style="width:100%" src="wp-content/uploads/2020/06/deep-fakes.jpg"></a>
      <a href="https://gijn.org/2018/05/28/what-to-watch-for-in-the-coming-wave-of-deep-fake-videos/"><b>What to look for in deep fake videos</b></a>
        <p>"Deep fakes" are videos that have been edited to show someone's head on the wrong person's body - mouth, words and all. This holds deep potential for misinformation in a few different ways, and unfortunately, defensive technology is far from catching up. Here is what reporters can do in the meantime.</p>
 <p>This article is also available in <a href="https://gijn.org/2018/10/30/%D1%87%D0%B5%D0%B3%D0%BE-%D0%B6%D0%B4%D0%B0%D1%82%D1%8C-%D0%BE%D1%82-%D0%B3%D1%80%D1%8F%D0%B4%D1%83%D1%89%D0%B5%D0%B9-%D0%B2%D0%BE%D0%BB%D0%BD%D1%8B-%D0%B3%D0%BB%D1%83%D0%B1%D0%BE%D0%BA%D0%B8/">Russian</a> and <a href="https://gijn.org/2018/05/28/%E0%A6%86%E0%A6%B8%E0%A6%9B%E0%A7%87-%E0%A6%A1%E0%A6%BF%E0%A6%AA-%E0%A6%AB%E0%A7%87%E0%A6%87%E0%A6%95-%E0%A6%AD%E0%A6%BF%E0%A6%A1%E0%A6%BF%E0%A6%93/">Bangla</a>.</p>
@@ -161,15 +161,15 @@
 <div class="row">
 
 	   <div style="width:100%" class="col-md-4">
-		   <a href="https://www.americanpressinstitute.org/publications/reports/strategy-studies/data-journalism/"><img src="https://www.samanthasunne.com/wp-content/uploads/2019/01/api5.jpg"></a>
+		   <a href="https://www.americanpressinstitute.org/publications/reports/strategy-studies/data-journalism/"><img src="wp-content/uploads/2020/06/api5.jpg"></a>
 		   <a href="https://www.americanpressinstitute.org/publications/reports/strategy-studies/data-journalism/"><b>How to start doing data journalism</b></a>
        <p>Most, if not all, newsrooms have realized that they want data in their reporting. But how do you go about it when your staff is already overworked? This strategy study examines case studies, both successful and unsuccessful.</p>
 <p>This paper has been recommended by the <a href="https://journalistsresource.org/studies/government/data-journalism-visualization-mapping-ethics-syllabus/">Harvard Shorenstein Center</a> and the <a href="https://www.knightdigitalmediacenter.org/news/2016/03/data-journalism-past-present-and-future.html">Knight Digital Media Center</a> as a teaching resource.</p>
      </div>
 
      <div class="col-md-4">
-		 <a href="https://www.samanthasunne.com/wp-content/uploads/2014/06/ire14_tipsheet.pdf"><img style="width:100%" src="https://www.samanthasunne.com/wp-content/uploads/2019/01/yale-university-1604158_1280.jpg"></a>
-		 <a href="https://www.samanthasunne.com/wp-content/uploads/2014/06/ire14_tipsheet.pdf"><b>Covering crimes on campus</b></a>
+		 <a href="wp-content/uploads/2020/06/ire14_tipsheet.pdf"><img style="width:100%" src="wp-content/uploads/2020/06/yale-university-1604158_1280.jpg"></a>
+		 <a href="wp-content/uploads/2020/06/ire14_tipsheet.pdf"><b>Covering crimes on campus</b></a>
        <p>University campuses provide a unique hunting ground for investigative stories. This tipsheet explains some of the most common and accessible sources for breaking in to these closed communities, including CLERY reports, Title IX complaints, FERPA exceptions and FBI UCR statistics.</p>
 <p>Warning: this tipsheet was published in 2014 and may be out of date.</p>
      </div>
