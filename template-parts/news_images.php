ef47d19904c76899230e0d3caf1171a05c133229
Lukas Udstuen <Lukas.Udstuen@gmail.com>
2020-06-27 22:16:28 -0300

    take out slash

--- template-parts/news_images.php
+++ template-parts/news_images.php
@@ -1,7 +1,7 @@
 <div class="news_banner news_div">
-  <img class="news_banner" src="/wp-content/uploads/2016/10/wapo.png" ></img>
-  <img class="news_banner" src="/wp-content/uploads/2016/09/reuters.png" ></img>
-  <img class="news_banner" src="/wp-content/uploads/2016/09/source.png" ></img>
-  <img class="news_banner" src="/wp-content/uploads/2016/09/api.png"</img>
-  <img class="news_banner" src="/wp-content/uploads/2016/09/npr.png" </img>
+  <img class="news_banner" src="wp-content/uploads/2016/10/wapo.png" ></img>
+  <img class="news_banner" src="wp-content/uploads/2016/09/reuters.png" ></img>
+  <img class="news_banner" src="wp-content/uploads/2016/09/source.png" ></img>
+  <img class="news_banner" src="wp-content/uploads/2016/09/api.png"</img>
+  <img class="news_banner" src="wp-content/uploads/2016/09/npr.png" </img>
 </div>
