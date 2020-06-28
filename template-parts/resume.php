551ac585e855cb6b6c0f65f7c4af42f0254fcc69
Lukas Udstuen <Lukas.Udstuen@gmail.com>
2020-06-27 23:07:28 -0300

    update resume

--- template-parts/resume.php
+++ template-parts/resume.php
@@ -9,7 +9,7 @@
   <div class="row">
 
       <div class="col-md-2" style="  display:table-cell;">
-        <img class="resume-icon-img" src="https://www.samanthasunne.com/wp-content/uploads/2019/01/chart1.png">
+        <img class="resume-icon-img" src="wp-content/uploads/2020/06/chart1.png">
       </div>
       <div class="col-md-4" style="vertical-align:top; ">
         <div style="float:right; display:inline;"><p>Extensive experience in public records, documents, data and backgrounding. Special familiarity with court records and the criminal justice system. Comfortable with Excel, SQL, web scraping and a <i>tiny</i> bit of R.
@@ -19,7 +19,7 @@
       </div>
 
       <div class="col-md-2" style="vertical-align:top;">
-        <img class="resume-icon-img" src="<?php echo get_template_directory_uri(); ?>/static/easel.png">
+        <img class="resume-icon-img" src="wp-content/uploads/2020/06/easel.png">
       </div>
       <div class="col-md-4" style="vertical-align:top;">
         <p>Almost five years experience in training students and reporters in hands-on, easy-to-use tools for journalism. Frequent speaker for IRE/NICAR, American University and SPJ/Google News Initiative.</p>
